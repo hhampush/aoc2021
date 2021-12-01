287aab551f1d138a316d67374b613eeb3e526082
Hampus Hessel <hessel.hampus@gmail.com>
2021-12-02 00:28:52 +0100

    Day 1, part 2, no longer one line, sad

--- day01/aoc.cpp
+++ day01/aoc.cpp
@@ -1,2 +1,8 @@
 #include <bits/stdc++.h>
-int main() {for (auto [f,l,p,c] = std::tuple{std::ifstream("input.txt"), std::string(""), 0, 0}; std::getline(f,l); c += (std::stoi(l) > p ? 1 : 0), p = std::stoi(l)) {std::cout << c << std::endl;}}
+using namespace std;
+int main(int argc, char* argv[], char *envp[]) {
+    const auto mod = (getenv("part") == nullptr || string(getenv("part")).find("part1") == string::npos) ? 3 : 1;
+    for (auto [a,f,l,i,p,s] = tuple{array<int,3>{0},ifstream("input.txt"), string(""), 0, INT_MAX, 0}; getline(f,l) && (a.at(i++ % mod) = stoi(l)); p = s, cout << argc - (mod == 3 ? 2 : 0) << endl) {
+        argc += ((s = (i >= 3 ? accumulate(begin(a), begin(a) + mod, 0) : 0)) > p ? 1 : 0);
+    }; 
+ }
