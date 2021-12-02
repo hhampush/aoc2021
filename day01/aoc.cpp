f2ac160a49b9857f9be9eec5efb2b489419d1834
Hampus Hessel <hessel.hampus@gmail.com>
2021-12-02 11:53:26 +0100

    remove a few lines

--- day01/aoc.cpp
+++ day01/aoc.cpp
@@ -3,6 +3,4 @@ using namespace std;
 int main(int argc, char* argv[], char *envp[]) {
     const auto mod = (getenv("part") == nullptr || string(getenv("part")).find("part1") == string::npos) ? 3 : 1;
     for (auto [a,f,l,i,p,s] = tuple{array<int,3>{0},ifstream("input.txt"), string(""), 0, INT_MAX, 0}; getline(f,l) && (a.at(i++ % mod) = stoi(l)); p = s, cout << argc - (mod == 3 ? 2 : 0) << endl) {
-        argc += ((s = (i >= 3 ? accumulate(begin(a), begin(a) + mod, 0) : 0)) > p ? 1 : 0);
-    }; 
- }
+        argc += ((s = (i >= 3 ? accumulate(begin(a), begin(a) + mod, 0) : 0)) > p ? 1 : 0); }; }
