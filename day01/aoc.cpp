779845d2a8fae5a37dc05bfdf2be451f51c5d1dd
Hampus Hessel <hessel.hampus@gmail.com>
2021-12-03 08:11:26 +0100

    wip

--- day01/aoc.cpp
+++ day01/aoc.cpp
@@ -1,4 +1,9 @@
 #include <bits/stdc++.h>
-using namespace std; int main(int argc, char* argv[]) { const auto mod = (getenv("part") == nullptr || string(getenv("part")).find("part1") == string::npos) ? 3 : 1;
-    for (auto [a,f,l,i,p,s] = tuple{array<int,3>{0},ifstream("input.txt"), string(""), 0, INT_MAX, 0}; getline(f,l) && (a.at(i++ % mod) = stoi(l)); p = s, cout << argc - (mod == 3 ? 2 : 0) << endl) {
-        argc += ((s = (i >= 3 ? accumulate(begin(a), begin(a) + mod, 0) : 0)) > p ? 1 : 0); }; }
+using namespace std;
+int main() {
+    int window_length = (getenv("part") == nullptr || string(getenv("part")).find("part1") == string::npos) ? 3 : 1, result = 0;
+    for (auto [window_arr,file,line,i,prev_sum,curr_sum] = tuple{array<int,3>{0},ifstream("input.txt"), string(""), 0, INT_MAX, 0}; getline(file,line) && (window_arr.at(i++ % window_length) = stoi(line)); prev_sum = curr_sum) {
+        result += ((curr_sum = (i >= window_length ? accumulate(begin(window_arr), begin(window_arr) + window_length, 0) : 0)) > prev_sum ? 1 : 0);
+    };
+    cout << result - (window_length > 1 ? 1 : 0);
+}
--- day02/aoc.cpp
+++ day02/aoc.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
-using namespace std; int main(int argc, char* argv[]) { int hor_pos = 0, depth = 0, aim = 0, part1 = (getenv("part") != nullptr && string(getenv("part")).find("part1") != string::npos);
-    for (auto [f,l,i,k] = tuple{ifstream("input.txt"), string(""), 0, string("")}; getline(f,l);) {
-        for (auto [ss,sl,p] = tuple{stringstream(l), string(""), string("")}; getline(ss, sl, ' ');) {try{ i = stoi(sl); } catch (exception& e){k = sl;}}
-        aim += (k.find("up") != string::npos ? -i : (k.find("down") != string::npos ? i : 0)); hor_pos += (k.find("forward") != string::npos ? i : 0);
-        if (!part1) depth += (k.find("forward") != string::npos ? i * aim : 0); else depth += (k.find("up") != string::npos ? -i : (k.find("down") != string::npos ? i : 0));}; cout << hor_pos * depth;}
+using namespace std;
+int main() {
+    int hor_pos = 0, depth = 0, aim = 0, part1 = (getenv("part") != nullptr && string(getenv("part")).find("part1") != string::npos);
+    for (auto [f,l,i,up,down,forward] = tuple{ifstream("input.txt"), string(""), 0, false, false, false}; getline(f,l);) {
+        for (auto [ss,sl,p] = tuple{stringstream(l), string(""), string("")}; getline(ss, sl, ' ');) {
+            if (!(all_of(sl.begin(), sl.end(), ::isdigit) && (i = stoi(sl)))) {
+                up = (sl.find("up") != string::npos), down = (sl.find("down") != string::npos), forward = (sl.find("forward") != string::npos);
+            }
+        }
+        aim = aim + (up ? -i : down ? i : 0), hor_pos = hor_pos + (forward ? i : 0);
+        depth = depth + (part1 ? (up ? -i : down ? i : 0) : (forward ? i * aim : 0));
+    };
+    cout << hor_pos * depth;
+}
