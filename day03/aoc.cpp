2ac115756d7f450d4fe6eb3c35a773dcd517b5c6
Hampus Hessel <hessel.hampus@gmail.com>
2021-12-04 01:40:42 +0100

    day03

---
+++ day03/aoc.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+void filter_list(std::vector<std::string>& lines, const char value, const int pos) { for (auto it = lines.begin(); it != lines.end(); it++) { if (it->at(pos) != value) lines.erase(it--); }}
+
+std::tuple<int,int> count_in_col(std::vector<std::string>& lines, const int col, std::string row_string = "") {
+    for (auto &line : lines) { row_string += line.at(col); }
+    return std::tuple<int,int>(count(row_string.begin(), row_string.end(), '0'), count(row_string.begin(), row_string.end(), '1')); }
+
+std::vector<std::string> check_scrub_power(std::vector<std::string> lines, const bool scrub_mode) {
+    for (int i = 0; i < 12 && lines.size() > 1; i++) {
+        if (auto nums = count_in_col(lines, i); get<1>(nums) >= get<0>(nums)) filter_list(lines, (scrub_mode ? '0' : '1'), i);
+        else filter_list(lines, (scrub_mode ? '1' : '0'), i);
+    } return lines; }
+
+int main() {
+    auto [gamma, epsilon, lines] = std::tuple{std::string(), std::string(), std::vector<std::string>{}};
+    for (auto [file, line] = std::tuple{std::ifstream("input.txt"), std::string()}; std::getline(file, line);) { lines.push_back(line.substr(0, 12)); }
+    for (int i = 0; i < 12; i++) { auto nums = count_in_col(lines, i); gamma.append(get<1>(nums) > get<0>(nums) ? "1" : "0"), epsilon.append(get<1>(nums) > get<0>(nums) ? "0" : "1"); }
+    std::cout << std::stoi(gamma, nullptr, 2) * std::stoi(epsilon, nullptr, 2) << std::endl << std::stoi(check_scrub_power(lines, false).at(0), nullptr, 2) * std::stoi(check_scrub_power(lines, true).at(0), nullptr, 2) << std::endl; }
