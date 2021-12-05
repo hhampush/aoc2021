b077c0e604a6cd4801b1c1f318e79fdb17cf6e07
Hampus Hessel <hessel.hampus@gmail.com>
2021-12-05 15:23:37 +0100

    day04

---
+++ day04/aoc.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+bool is_winner(std::array<std::array<std::tuple<int, bool>, 5>, 5> &board, int x, int y, bool mode, bool winner = true) {
+    for (int i = 0; i < 5; i++) if (get<1>(mode ? board.at(x).at(i) : board.at(i).at(y)) != true) winner = false;
+    return winner;
+}
+
+int unmarked_sum(std::array<std::array<std::tuple<int, bool>, 5>, 5> &board, int sum = 0) {
+    for (int x = 0; x < 5; x++)
+        for (int y = 0; y < 5; y++) if (get<1>(board.at(x).at(y)) != true) sum += get<0>(board.at(x).at(y));
+    return sum;
+}
+
+bool mark_in_board(std::vector<std::array<std::array<std::tuple<int, bool>, 5>, 5>> &boards, int num, int board, std::unordered_set<int> &winners)
+{
+    for (int y = 0; y < 5; y++)
+        for (int x = 0; x < 5; x++) {
+            if (!get<1>(boards.at(board).at(x).at(y)) && get<0>(boards.at(board).at(x).at(y)) == num) get<1>(boards.at(board).at(x).at(y)) = true;
+            if (is_winner(boards.at(board), x, y, true) || is_winner(boards.at(board), x, y, false)) 
+                return get<1>(winners.insert(board));
+        }
+    return false;
+}
+
+int main() {
+    auto [boards, draw_pool, file, first_line, winners] = std::tuple{std::vector<std::array<std::array<std::tuple<int, bool>, 5>, 5>>(), std::vector<int>(), std::ifstream("input.txt"), std::string(""), std::unordered_set<int>()};
+    getline(file, first_line);
+    const auto view = first_line | std::ranges::views::split(',') | std::ranges::views::transform([]( auto &&str) { return std::string_view(&*str.begin(), std::ranges::distance(str)); });
+    for (auto &&word : view) draw_pool.push_back(std::atoi(word.data()));
+
+    for (auto [line, board, x, y] = std::tuple{std::string(), std::array<std::array<std::tuple<int, bool>, 5>, 5>(), 0, 0}; !file.eof() && file >> line;) {
+        board.at(x++).at(y) = {std::stoi(line), false};
+        if (x == 5) {x = 0; y++;};
+        if (y == 5) {boards.push_back(board); x = 0, y = 0;};
+    }
+
+    for (auto &num : draw_pool)
+        for (int unsigned i = 0; i < boards.size(); i++)
+            if (!winners.contains(i) && mark_in_board(boards, num, i, winners)) std::cout << "winner in " << i << " : " << unmarked_sum(boards.at(i)) * num << std::endl;
+}
